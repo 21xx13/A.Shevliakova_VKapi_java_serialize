e4809eab55f99bc56c3d2d36a512f13da7432e16
rabbit21xx13 <rabbit21xx13@users.noreply.github.com>
2020-11-22 19:39:09 +0500

    to .json file by dictionary + write to DB

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,7 @@ import java.io.*;
 import java.net.URL;
 import java.net.URLConnection;
 import java.nio.file.*;
+import java.sql.*;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -29,10 +30,63 @@ public class Main {
         JSONObject json = new JSONObject(inputLine);
         int count = (int) json.getJSONObject("response").get("count");
         JSONArray friends = json.getJSONObject("response").getJSONArray("items");
-        //заполнение словаря
+        //заполнение словаря данными
         Map<String, CityInfo> filledDict = fillDictionary(friends, count);
         System.out.println(jsonFromDict(filledDict).toString());
         createJSONFile(jsonFromDict(filledDict).toString());
+
+
+        // ==== Чтение только что созданного файла и запись в БД ====
+        JSONObject dataToDB = readJsonFile();
+        JSONArray arr = (JSONArray) dataToDB.get("Data");  //извлечение массива с городами
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
+            Connection conn = getConnection(); // подключение к БД
+            //Statement statement = conn.createStatement();
+            //createTable(statement);  // создание новой таблицы
+            String sqlInsert = "INSERT City(CityName, Percent, CountFriends) VALUES (?, ?, ?)";
+            PreparedStatement preparedStatement = conn.prepareStatement(sqlInsert); // шаблон запроса SQL
+            // обход массива с данными из файла
+            for (int i = 0; i < arr.length(); i++) {
+                JSONObject item = (JSONObject) arr.get(i);
+                //заполнение шаблона
+                preparedStatement.setString(1, item.getString("City"));
+                preparedStatement.setDouble(2, item.getDouble("Percent"));
+                preparedStatement.setInt(3, item.getInt("Amount"));
+                // выполнение запроса
+                preparedStatement.executeUpdate();
+            }
+            conn.close();
+        } catch (Exception ignored) {
+        }
+    }
+
+    private static JSONObject readJsonFile() throws IOException, JSONException {
+        try (BufferedReader reader = new BufferedReader(new FileReader("src/data.json"))) {
+            return new JSONObject(reader.readLine());
+        }
+    }
+
+
+    private static void createTable(Statement statement) throws SQLException {
+        String sqlCommand = "CREATE TABLE City (Id INT PRIMARY KEY AUTO_INCREMENT, CityName VARCHAR(20), " +
+                "Percent DOUBLE, CountFriends INT)";
+        statement.executeUpdate(sqlCommand);
+    }
+
+
+    public static Connection getConnection() throws SQLException {
+        String[] connectData = new String[3];
+        try {
+            File file = new File("src/database_properties.txt");
+            FileReader fr = new FileReader(file);
+            BufferedReader reader = new BufferedReader(fr);
+            for (int i = 0; i < 3; i++)
+                connectData[i] = reader.readLine();
+        } catch (Exception ignored) {
+        }
+        return DriverManager.getConnection(connectData[0], connectData[1], connectData[2]);
     }
 
     private static Map<String, CityInfo> fillDictionary(JSONArray friends, int countFriends) {
@@ -52,24 +106,6 @@ public class Main {
         return result;
     }
 
-    //чтение ответа api
-    private static String readAll(Reader rd) throws IOException {
-        StringBuilder sb = new StringBuilder();
-        int cp;
-        while ((cp = rd.read()) != -1) {
-            sb.append((char) cp);
-        }
-        return sb.toString();
-    }
-
-    //создание файла .json
-    private static void createJSONFile(String data) throws IOException {
-        Files.createFile(Paths.get("src/data.json"));
-        FileWriter fw = new FileWriter("src/data.json");
-        fw.write(data);
-        fw.close();
-    }
-
     public static JSONObject jsonFromDict(Map<String, CityInfo> cityDict) throws JSONException {
         JSONArray array = new JSONArray();
         String[] keyArray = cityDict.keySet().toArray(new String[0]);
@@ -80,5 +116,24 @@ public class Main {
         }
 
         return new JSONObject().put("Data", array);
+    }
+
+    //создание файла .json
+    private static void createJSONFile(String data) throws IOException {
+        Files.createFile(Paths.get("src/data.json"));
+        FileWriter fw = new FileWriter("src/data.json");
+        fw.write(data);
+        fw.close();
+    }
+
+
+    //чтение ответа api
+    private static String readAll(Reader rd) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        int cp;
+        while ((cp = rd.read()) != -1) {
+            sb.append((char) cp);
+        }
+        return sb.toString();
     }
 }
